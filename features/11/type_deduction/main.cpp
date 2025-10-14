c7a2b566f8229cb1051a2844c61c8881349674a5
Simeon Ehrig <s.ehrig@hzdr.de>
2025-10-14 16:53:38 +0200

    extent type deduction example with static arrays

--- features/11/type_deduction/main.cpp
+++ features/11/type_deduction/main.cpp
@@ -71,5 +71,20 @@ int main(int argc, char **argv) {
   h4(Foo{}, "Foo{}");                 // h4<Foo>(Foo &&)
   h4((Foo const){}, "(Foo const){}"); // h4<Foo const>(Foo const &&)
 
+  Foo const const_f_static_array[2][2];
+  Foo mut_f_static_array[2][2];
+
+  h1(const_f_static_array, "const_f_static_array");   // h1<const Foo (*)[2]>(const Foo (*)[2])
+  h1(mut_f_static_array, "mut_f_static_array");       // h1<Foo (*)[2]>(Foo (*)[2])
+
+  h2(const_f_static_array, "const_f_static_array");   // h2<const Foo (*)[2]>(const Foo (*const)[2])
+  h2(mut_f_static_array, "mut_f_static_array");       // h2<Foo (*)[2]>(Foo (*const)[2])
+
+  h3(const_f_static_array, "const_f_static_array");   // h3<const Foo[2][2]>(const Foo (&)[2][2])
+  h3(mut_f_static_array, "mut_f_static_array");     // h3<Foo[2][2]>(Foo (&)[2][2])
+
+  h4(const_f_static_array, "const_f_static_array");   // h4<const Foo (&)[2][2]>(const Foo (&)[2][2])
+  h4(mut_f_static_array, "mut_f_static_array");     // h4<Foo (&)[2][2]>(Foo (&)[2][2])
+
   return 0;
 }
